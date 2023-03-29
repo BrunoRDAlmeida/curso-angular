3443eed72996c6fa7cc901a88065f6bd51d94e7f
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-03-29 19:28:55 +0200

    Aula 4 , Pokedex

--- pokedex/src/app/pages/pages.module.ts
+++ pokedex/src/app/pages/pages.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 //Module Routing
 import { RoutingModule } from './routing.module';
 
+//Module
+import { SharedModule } from '../shared/shared.module';
+
 //Pages
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
-
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
