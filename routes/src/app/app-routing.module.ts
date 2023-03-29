0ebdb9b3e2bb118231c185df88d52acfa0817fea
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-03-29 12:27:13 +0200

    correção aula 79

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { SobreComponent } from './pages/sobre/sobre.component';
 const routes: Routes = [
   {path: "", component: HomeComponent, pathMatch: 'full'},
   {path: "sobre", component: SobreComponent, children:[
-    {path: "sobre/:id/:username", component: SobreComponent}
+    {path: "bruno", component: SobreComponent}
   ]},
   {path: "404", component: PageErrorComponent},
   {path: "**", redirectTo: '404'}
