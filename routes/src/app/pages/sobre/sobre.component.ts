bc32238d4c17ee38efa8fbedfb93189e9281681d
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-03-29 12:08:13 +0200

    Aula 77 Parametros nas Rotas

--- routes/src/app/pages/sobre/sobre.component.ts
+++ routes/src/app/pages/sobre/sobre.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
   styleUrls: ['./sobre.component.scss']
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit{
+  
+  constructor(private ActivatedRoute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.ActivatedRoute.params.subscribe(
+      res => console.log(res['id'], res['username'], res)
+    )
+    this.ActivatedRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
 }
