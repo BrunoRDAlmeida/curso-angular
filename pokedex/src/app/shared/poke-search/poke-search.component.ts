cbb574b610b648486b95a8ead465207b085190e7
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-03-30 18:30:00 +0200

    Aula 11 Pokedex

--- pokedex/src/app/shared/poke-search/poke-search.component.ts
+++ pokedex/src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'poke-search',
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 
 export class PokeSearchComponent implements OnInit{
 
+  @Output() public emmitSearch:EventEmitter<string> = new EventEmitter();
+  
   constructor() {}
 
   ngOnInit(): void {
     
+  }
+
+  public search(value: string){
+    this.emmitSearch.emit(value);
   }
 }
