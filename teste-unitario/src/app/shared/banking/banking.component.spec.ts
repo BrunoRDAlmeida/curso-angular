30160895d03453f46de18a4eb634aef4ec8789c1
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-04 13:36:57 +0200

    com erro Aula 10 Testes Unitarios

--- teste-unitario/src/app/shared/banking/banking.component.spec.ts
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { finalize } from 'rxjs';
+import { ListComponent } from '../investiments/list/list.component';
 
 import { BankingComponent } from './banking.component';
 
@@ -9,7 +10,7 @@ describe('BankingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BankingComponent ]
+      declarations: [ BankingComponent, ListComponent ]
     })
     .compileComponents();
 
