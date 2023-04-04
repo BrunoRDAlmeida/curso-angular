1b643ccc29d62514de0419c0668b879383f0892e
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-04 22:02:59 +0200

    Aulas de Testes  Unitarios Completas

--- teste-unitario/src/app/shared/investments/services/list-investments.service.ts
+++ teste-unitario/src/app/shared/investments/services/list-investments.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 //Interface
 import { Investments } from '../model/investments';
@@ -8,7 +8,7 @@ import { Investments } from '../model/investments';
 @Injectable({
   providedIn: 'root'
 })
-export class ListInvestmentsService {
+export class ListInvestmentsService{
 
   private url: string = 'https://raw.githubusercontent.com/troquatte/fake-server/main/investiments-all.json';
 
@@ -17,8 +17,6 @@ export class ListInvestmentsService {
   ) {}
 
   public list():Observable<Array<Investments>>{
-    return this.http.get<Array<Investments>>(this.url).pipe(
-      map(res => res)
-    )
+    return this.http.get<Array<Investments>>(this.url)
   };
 }
