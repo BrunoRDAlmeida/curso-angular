bd5c090f520fa0132629f4c9829305b19b930b0a
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-04 11:23:22 +0200

    Aula 7 Testes Unitarios

--- teste-unitario/src/app/shared/banking/banking.component.spec.ts
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -20,4 +20,27 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`(U) getPoupanca(): shoud have poupanca = 10`, () => {
+    expect(component.getPoupanca).toEqual(10);
+  });
+
+  it (`(U) getCarteira(): shoud have carteira = 50`, () => {
+    expect(component.getCarteira).toEqual(50);
+  });
+
+  it (`(U) setSacar(): shoud transfer poupanca from carteira`, () => {
+    component.setSacar('10');
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+  
+  it (`(U) setDepositar(): shoud transfer carteira from poupanca`, () => {
+    component.setSacar('50');
+    expect(component.getCarteira).toEqual(0);
+    expect(component.getPoupanca).toEqual(60);
+  });
+  
+  //it (`(U) getCarteira(): shoud have carteira = 50`, () => {});
+
 });
