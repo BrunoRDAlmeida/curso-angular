43a43b33c1e7b756b849e9d7e6efae09c54de354
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-04 10:56:46 +0200

    Aula 6 Testes Unitarios

--- teste-unitario/src/app/shared/banking/banking.component.ts
+++ teste-unitario/src/app/shared/banking/banking.component.ts
@@ -23,15 +23,24 @@ export class BankingComponent implements OnInit{
     return this.carteira;
   }
 
-  public setSacar(value: string): number {
+  public setSacar(value: string): number | undefined {
     const sacar = Number(value);
-    console.log(sacar)
-    return sacar;
+
+    if(isNaN(sacar) || this.poupanca < sacar){
+      return;
+    }
+    this.poupanca -= sacar;
+    return (this.carteira += sacar);
   }
 
-  public setDepositar(value: string): number {
+  public setDepositar(value: string): number | undefined{
     const depositar = Number(value);
-    return depositar;
+
+    if(isNaN(depositar) || this.carteira < depositar){
+      return;
+    }
+    this.carteira -= depositar;
+    return (this.poupanca += depositar);
   }
 
 }
