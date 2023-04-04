1914c21fde37a4ac5d12fa8cb1701ac3a005e4ea
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-04 18:27:34 +0200

    Aula 13 Testes Unitarios

--- teste-unitario/src/app/shared/investments/services/list-investments.service.spec.ts
+++ teste-unitario/src/app/shared/investments/services/list-investments.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 import { ListInvestmentsService } from './list-investments.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('ListInvestmentsService', () => {
   let service: ListInvestmentsService;
 
+  let httpTestingController: HttpTestingController;
+
+  let httpClient: HttpClient;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ListInvestmentsService);
   });
 
