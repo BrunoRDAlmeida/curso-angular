14ea7c6dbb7a3e14bc5fe361807521a97d1c8ffc
Luis Henrique Silveira da Rocha <henrique.srocha@gmail.com>
2023-04-05 23:53:20 +0200

    paginacao basica no pokedex

--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -3,49 +3,48 @@ import { Injectable } from '@angular/core';
 import { map, Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokeApiService {
-
   // private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
   private url: string = 'https://pokeapi.co/api/v2/pokemon/';
 
   // configurando query parameter para paginação
 
   // offset é a partir de que pokemon é pra mostrar
-  private pokemonAPartirDe: number = 0
+  private pokemonAPartirDe: number = 0;
 
   // limite de pokemons por pagina
-  private limiteDePokemonsPorPagina: number = 100
+  private limiteDePokemonsPorPagina: number = 100;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
-  get apiListAllPokemon(): Observable<any> {
-    return this.http.get<any>(this.url, {
-      params: {
-        offset: this.pokemonAPartirDe,
-        limit: this.limiteDePokemonsPorPagina
-      }
-    }).pipe(
-      tap(res => res),
-      tap(res => {
-        res.results.map((resPokemons: any) => {
-
-          this.apiGetPokemons(resPokemons.url).subscribe(
-            res => resPokemons.status = res
-          )
-        })
+  /**
+   * Retorna a lista de pokemons (paginada)
+   * @param start  o pokemon inicial a ser mostrado (começa do zero)
+   * @param limitPerPage (opcional) quantidade de pokemons a serem retornados
+   * @returns a lista de pokemons
+   */
+  public getPagedPokemons(start = 0, limitPerPage = 100): Observable<any> {
+    return this.http
+      .get<any>(this.url, {
+        params: {
+          offset: start,
+          limit: limitPerPage,
+        },
       })
-    )
+      .pipe(
+        tap((res) => {
+          res.results.map((resPokemons: any) => {
+            this.apiGetPokemons(resPokemons.url).subscribe(
+              (res) => (resPokemons.status = res)
+            );
+          });
+        })
+      );
   }
 
   public apiGetPokemons(url: string): Observable<any> {
-    return this.http.get<any>(url).pipe(
-      map(
-        res => res
-      )
-    )
+    return this.http.get<any>(url).pipe(map((res) => res));
   }
 }
