43887b3c01818d564be502198c2eef033a33d7ce
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-06 17:12:54 +0200

    Aula 4 Login

--- projeto-pratico/curso-angular-auth-front/src/app/app.component.ts
+++ projeto-pratico/curso-angular-auth-front/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  template: `<a [routerLink]="['admin']">admin</a>"<router-outlet></router-outlet>`,
 })
 export class AppComponent {
+  
 }
