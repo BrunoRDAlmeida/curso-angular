a34a6533947f4a08596f3e718654dd689b318cc1
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-12 13:04:40 +0200

    Aula Login completa

--- projeto-pratico/curso-angular-auth-front/src/app/components/admin/pages/home/home.component.ts
+++ projeto-pratico/curso-angular-auth-front/src/app/components/admin/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void { }
+
+  public logout() {
+    this.authService.logout();
+  }
 }
--- projeto-pratico/curso-angular-auth-front/src/app/core/components/auth/auth.module.ts
+++ projeto-pratico/curso-angular-auth-front/src/app/core/components/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignComponent } from './pages/sign/sign.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { SignComponent } from './pages/sign/sign.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
