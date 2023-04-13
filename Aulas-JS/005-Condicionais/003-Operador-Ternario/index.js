64b438c8e2af5436104fcf992bc1037488317212
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-13 19:13:28 +0200

    Aulas de JS

---
+++ Aulas-JS/005-Condicionais/003-Operador-Ternario/index.js
@@ -0,0 +1,14 @@
+const velocidade = 75;
+const warn = 85;
+
+//condição ? true : false;
+
+const condicao = velocidade >= warn
+    ? console.log("Recebeu uma multa!")
+    : console.log("Continue andando!");
+
+// if (velocidade >= warn) {
+//     console.log("Recebeu uma multa!");
+// }else{
+//     console.log("Continue andando!");
+// }
