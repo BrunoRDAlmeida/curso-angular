4abeb7bc1f39d655856b9ef9fe5a50363e8590a8
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-13 12:51:27 +0200

    Aulas de JS

---
+++ Aulas-JS/002-Tipos-Primitivos/001-String/index.js
@@ -0,0 +1,12 @@
+console.log('"Olá Mundo!"');
+console.log("Olá Mundo!");
+console.log(`Olá Mundo!`);
+
+
+console.log(String(123456)); //transforma os numeros(number) em texto(string)
+/*
+    Como podemos escrever uma string
+    'Aqui vai uma String' usado quando quero que o texto fique com aspas duplos
+    "Aqui vai uma String" usado mais quando só tem texto
+    `Aqui vai uma String` usado quando tem texto e variavel
+*/
