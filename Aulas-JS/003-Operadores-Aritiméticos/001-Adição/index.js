0b7abc87cd08360abd2e6e764cec9ce658395e6a
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-13 17:10:33 +0200

    Aulas de JS

---
+++ Aulas-JS/003-Operadores-Aritiméticos/001-Adição/index.js
@@ -0,0 +1,6 @@
+console.log(1 + 1);
+console.log(25 + 50);
+
+console.log("Bruno " + "Rocha");
+console.log("Nome: " + "Bruno" + " Rocha" + ", " + "idade: " + 29 + 1);
+console.log("Nome: " + "Bruno" + " Rocha" + ", " + "idade: " + (29 + 1));
---
+++ Aulas-JS/003-Operadores-Aritiméticos/005-Atribuição/index.js
@@ -0,0 +1,19 @@
+let a = 6;
+let b = 2;
+
+//console.log(a);
+//sonsole.log(a + b);
+
+a += 1;
+// a = a + 1
+
+a -= 1;
+// a = a - 1
+
+a *= a;
+// a = a * 2
+
+a /= 3;
+//a = a / 3
+
+consolw.log(a);
