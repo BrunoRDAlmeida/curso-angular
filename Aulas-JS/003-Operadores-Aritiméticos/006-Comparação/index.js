eb9dcad9b52c65afb92ac6235e1f702938b5fbd1
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-13 17:21:50 +0200

    Aulas de JS

---
+++ Aulas-JS/003-Operadores-Aritiméticos/006-Comparação/index.js
@@ -0,0 +1,17 @@
+console.log(1 == 1);
+console.log(1 == "1");
+console.log(1 === 1);
+console.log(1 === "1");
+
+console.log(1 != "1");
+console.log(1 !== 1);
+
+console.log(3 > 2);
+console.log(3 < 2);
+
+console.log(2 >= 2);
+console.log(1 <= 2);
+
+/*
+    == , === , != , !== , > , < , >= , <=
+*/
