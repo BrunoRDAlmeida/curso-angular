6ca8d55dd8aab35f1d53cb08eff72cb0579a0305
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-13 16:23:57 +0200

    Aulas de JS

---
+++ Aulas-JS/002-Tipos-Primitivos/006-Object/index.js
@@ -0,0 +1,61 @@
+/*
+    Objeto: Tenis (Propriedade) "cadarço", "palmilha"
+    Objeto: Celular (Propriedade)
+    Objeto: Drone (Propriedade)
+
+    Propriedade / Atributo / Funcionalidades
+*/
+
+console.log({
+    tenis: [
+        {
+            preco: 150,
+            disponivel: true,
+            estoque: 150,
+            cadarco: {
+                cor: "rosa",
+                tamanho: "G",
+            },
+            palmilha: {
+                tamnho: 43,
+                cor: "roxo",
+            },
+        },
+        {
+            preco: 30,
+            disponivel: true,
+            estoque: 5,
+            cadarco: {
+                cor: "rosa",
+                tamanho: "G",
+            },
+            palmilha: {
+                tamnho: 43,
+                cor: "roxo",
+            },
+        },
+    ],
+});
+
+console.log([
+    {
+        tenis: {
+            preco: 150,
+            disponivel: true,
+            estoque: 150,
+            cadarco: {
+                cor: "rosa",
+                tamanho: "G",
+            },
+            palmilha: {
+                tamnho: 43,
+                cor: "roxo",
+            },
+        },
+    },
+    {
+        celular: {
+            marca: "Motorola",
+        }
+    }
+]);
