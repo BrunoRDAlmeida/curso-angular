9c9e7fdb334ada1ad704cda7d7b903800055eda2
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-13 17:37:29 +0200

    Aulas de JS

---
+++ Aulas-JS/003-Operadores-Aritiméticos/007-Lógicos/index.js
@@ -0,0 +1,12 @@
+console.log(1 === 1 && 2 >= 1);
+console.log(1 === 1 && 1 >= 2);
+
+console.log(1 === 1 || 1 === 1);
+
+console.log(!true);
+
+console.log(!false);
+
+/*
+    && ( E ) , || ( OU ) , ! ( NEGAÇÃO )
+*/
