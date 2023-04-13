42b78078f91b20d21b235e246921ceade8cc5543
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-13 18:48:29 +0200

    Aulas de JS

---
+++ Aulas-JS/005-Condicionais/001-if-else/index.js
@@ -0,0 +1,15 @@
+let soma = 0;
+
+if(soma >=0){
+    console.log("Você esta com o saldo positivo")
+}else{
+    console.log("Você esta com o saldo negativo")
+}
+
+// if(1 === 0){
+
+// }else if(1 === 1){
+
+// }else{
+
+// }
