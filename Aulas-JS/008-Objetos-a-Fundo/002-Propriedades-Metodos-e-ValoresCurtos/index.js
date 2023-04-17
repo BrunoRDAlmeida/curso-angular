b0f04f33dd5ed64e5e62bc7d39cd3524306cbb70
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-17 20:23:50 +0200

    Aulas de JS

---
+++ Aulas-JS/008-Objetos-a-Fundo/002-Propriedades-Metodos-e-ValoresCurtos/index.js
@@ -0,0 +1,28 @@
+// function Tenis(tamanho, estoque, preco){
+//     return{
+//         tamanho,
+//         estoque,
+//         preco
+//     };
+// }
+// console.log(Tenis(35, true, "R$ 40"));
+
+// const tamanho = 35;
+// const estoque = true;
+// const preco = "R$ 40";
+
+// const Tenis2 = {
+//     tamanho,
+//     estoque,
+//     preco
+// };
+
+// console.log(Tenis2);
+
+const Tenis3 = {
+    getTamanho(){
+        return tamanho = 35;
+    },
+};
+
+console.log(Tenis3.getTamanho());
