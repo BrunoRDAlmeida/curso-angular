04db1f9586f0ca992848877e76d993ea458eedcd
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-17 17:10:43 +0200

    Aulas de JS

--- Aulas-JS/007-Funções/007-Funções-Construtoras/index.js
+++ Aulas-JS/007-Funções/007-Funções-Construtoras/index.js
@@ -13,11 +13,14 @@ console.log(bruno.sobrenome());
 
 function Calculadora() {
     this.soma = (num1, num2) => {
-        return `${num1} ${num2}`;
-    }
-};
+        return `${num1 + num2}`;
+    };
+    this.subtracao = (num1, num2) => {
+        return `${num1 - num2}`;
+    };
+}
 
 this.subtracao = new Calaculadora();
 
-console.log(calculadora.soma)(1, 2);
-console.log(calculadora.subtracao)(5, 2);
+console.log(calculadora.soma(1, 2));
+console.log(calculadora.subtracao(5, 1));
