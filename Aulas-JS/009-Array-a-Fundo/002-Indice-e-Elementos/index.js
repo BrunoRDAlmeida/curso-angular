a355353598b5dc25ea0ccd179c2cd52ecfec602c
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-19 13:22:29 +0200

    Aulas de JS

---
+++ Aulas-JS/009-Array-a-Fundo/002-Indice-e-Elementos/index.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 3, 4, true, "Bruno", {nome: "Bruno"}];
+
+console.table(arr); //mostra uma tabela do meu array
+console.log(arr[6]); //mostra o objeto dentro do meu array na pocisão [ 6 ]
+console.log(arr.length); //mostra quantos objetos tem dentro do meu array
