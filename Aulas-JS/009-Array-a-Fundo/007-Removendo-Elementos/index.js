07e8d7ad5e6863bf82941289d14fe83fa5d05c18
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-19 16:12:31 +0200

    Aulas de JS

---
+++ Aulas-JS/009-Array-a-Fundo/007-Removendo-Elementos/index.js
@@ -0,0 +1,13 @@
+/*
+    shift = remove o perimeiro elemento de um array e retorna esse elemento
+    pop = remova o ultimo elemento de um array e retorna esse elemento
+*/
+
+let arr = [1,2,3,4,5];
+
+console.table(arr);
+
+console.log("shift =>", arr.shift());
+console.log("pop =>", arr.pop());
+
+console.table(arr);
