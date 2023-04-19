9c00569ab7f91534dd23dd2f80a4a6a29d1e8762
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-19 12:26:13 +0200

    Aulas de JS

---
+++ Aulas-JS/008-Objetos-a-Fundo/009-Clonagem-de-Objetos-Rasos-e-Profundos/index.js
@@ -0,0 +1,25 @@
+let Tenis = {
+    tamnho: 45,
+    estoque: true,
+};
+
+let link = { link: { a: "a", b: { c: "c" } } };
+
+function cloneObject(Object) {
+    return JSON.parse(JSON.stringify(Object));
+}
+
+let clone1 = Tenis;
+console.log(1, clone1)
+console.log(2, Tenis)
+
+let mesclar1 = Object.assign(cloneObject(Tenis), cloneObject(link));
+let mesclar2 = { ...cloneObject(Tenis), ...cloneObject(link) };
+
+clone1.estoque = false;
+mesclar1.link.a = "ABC";
+
+console.log(3, clone1);
+console.log(4, Tenis);
+console.log(5, mesclar1);
+console.log(6, mesclar2);
