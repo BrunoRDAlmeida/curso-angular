d196193966e13a5819b773face038c103c684010
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-22 15:56:26 +0200

    Aulas de JS Manipulando Style

---
+++ Aulas-JS/010-DOM/004-Manipulando-Style/001-Style/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const h1 = document.querySelector("h1")
+
+h1.style.fontFamily = "Arial"
+h1.style.color = "red"
+
+const p = document.querySelectorAll("p")
+
+p.forEach((element) => {
+    element.style.fontFamily = "Arial"
+    element.style.fontSize = "12px"
+})
