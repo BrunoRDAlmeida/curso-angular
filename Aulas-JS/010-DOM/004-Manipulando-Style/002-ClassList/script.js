1e8a6b1fab53343fa7eba58d087acc9bc7b6fc67
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-22 16:17:48 +0200

    Aulas de JS ClassList

---
+++ Aulas-JS/010-DOM/004-Manipulando-Style/002-ClassList/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const h1 = document.querySelector("h1")
+
+h1.classList.add("active", "Teste1", "Teste2")
+h1.classList.remove("Teste1", "Teste2")
+h1.classList.toggle("Teste2");
+
+if (h1.classList.contains("active")) {
+    alert("Existe a classe active")
+}
+
+console.log("h1")
