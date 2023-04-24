fb70d9358a17e9abac391aef9806b570e26cf195
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-24 12:42:41 +0200

    Aulas de JS insertBefore

---
+++ Aulas-JS/010-DOM/005-Encontrando-Elementos/007-insertBefore/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const container = document.querySelector("container")
+const paragrafo = document.querySelector("#paragrafo")
+const h1 = document.querySelector("h1")
+
+const newH2 = document.createElement("h2")
+newH2.innerText = "novo H2"
+
+container.insertBefore(newH2, paragrafo)
+
