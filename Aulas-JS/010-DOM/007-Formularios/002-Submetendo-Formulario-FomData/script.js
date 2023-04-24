bc6cdc05fae8ee4dc504278a3df96f1f65c608b9
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-24 18:40:26 +0200

    Aulas de JS Submetendo Formularios FormData

---
+++ Aulas-JS/010-DOM/007-Formularios/002-Submetendo-Formulario-FomData/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const form = document.forms.namedItem("registration")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const name = form.name.values;
+    const password = form.password.values;
+
+    FormData.set("name", name)
+    FormData.set("password", password)
+
+    FormData.log(FormData.has("name", name))
+    FormData.log(FormData.has("password", password))
+
+    FormData.log(FormData.get("name", name))
+    FormData.log(FormData.get("password", password))
+
+    //Delete
+    FormData.delete("name")
+    console.log(FormData.has("name"))
+
+    if (name && password) {
+        console.log(name, password)
+        form.submit()
+    }
+})
