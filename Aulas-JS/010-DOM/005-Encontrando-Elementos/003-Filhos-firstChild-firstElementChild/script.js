49e445064e119a3906a02bce998fd671c7d3095a
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-24 11:34:25 +0200

    Aulas de JS Filhos firstChild e firstElementChild

---
+++ Aulas-JS/010-DOM/005-Encontrando-Elementos/003-Filhos-firstChild-firstElementChild/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const container = document.querySelector(".container")
+
+const p = document.querySelector(".paragrafo")
+
+console.log(p.firstChild)
+console.log(p.firstElementChild)
