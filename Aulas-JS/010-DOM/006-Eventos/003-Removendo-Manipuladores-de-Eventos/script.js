baa628b1cc9f435a4feefb8a1a81253f0e05f5bf
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-24 14:38:05 +0200

    Aulas de JS Removendo Manipuladores de Evento

---
+++ Aulas-JS/010-DOM/006-Eventos/003-Removendo-Manipuladores-de-Eventos/script.js
@@ -0,0 +1,16 @@
+"uso strict";
+
+const btn = document.querySelector("button")
+
+const acionarAlert = () => {
+    let validador = false
+    alert(123)
+
+    validador = true
+
+    if (validador) {
+        btn.removeEventListener("click", acionarAlert)
+    }
+};
+
+btn.addEventListener("click", acionarAlert)
