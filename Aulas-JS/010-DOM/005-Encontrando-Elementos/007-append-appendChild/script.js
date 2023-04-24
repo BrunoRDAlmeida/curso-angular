964456a4e74fa6a8406ff5915bbf5db129ea0ec1
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-24 12:27:14 +0200

    Aulas de JS append appendChild

---
+++ Aulas-JS/010-DOM/005-Encontrando-Elementos/007-append-appendChild/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const container = document.querySelector("container")
+// container.append("<p> Olá Mundo </p>")
+
+const newDiv = document.createElement("div")
+newDiv.innerText
+
+containder.appendChild(newDiv);
