0cd14817c684c99c434cb199051af37d8ac8b899
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-24 11:39:50 +0200

    Aulas de JS Filhos lastChild e lastElementChild

---
+++ Aulas-JS/010-DOM/005-Encontrando-Elementos/004-Filhos-lastChild-lastElementChild/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const container = document.querySelector(".container")
+
+const p = document.querySelector(".paragrafo")
+
+console.log(p.lastChild)
+console.log(p.lastElementChild)
