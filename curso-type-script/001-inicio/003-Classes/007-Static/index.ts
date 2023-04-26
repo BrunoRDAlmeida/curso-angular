468d4215f7837258e099738f94c5b4182b327e61
Bruno Rocha de Almeida <brunolineage12@gmail.com>
2023-04-26 23:09:25 +0200

    Aulas de TS Static

---
+++ curso-type-script/001-inicio/003-Classes/007-Static/index.ts
@@ -0,0 +1,30 @@
+/*
+    A palavra chave static define um método estático para a classe
+    Métodos estáticos não são chamados na instância da classe
+    Em vez disso, eles são chamados na própria classe
+    Geralmente, são funções utilitárias, como funções para criar ou clonar objetos
+*/
+
+class Utils {
+    static cloneObject(object: Array<{}>) {
+        return JSON.parse(JSON.stringify(Object(object)))
+    }
+}
+
+class Pessoa{
+    public variasPessoas(){
+        Utils.cloneObject([])
+    }
+}
+
+const tenis1: { tamanho: number; estoque: boolean} ={
+    tamanho: 41,
+    estoque: true
+}
+
+const tenis2: { tamanho: number; estoque: boolean} ={
+    tamanho: 41,
+    estoque: true
+}
+
+console.log(Utils.cloneObject([tenis1,tenis2]))
